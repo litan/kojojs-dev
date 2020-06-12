64fd4488ee94299da68ccdc45193124014a7d544
Lalit Pant <pant.lalit@gmail.com>
2020-06-12 15:51:59 +0530

    Put all pic sequences in picSequences.scala.

---
+++ src/main/scala/kojo/picSequences.scala
@@ -0,0 +1,120 @@
+package kojo
+
+import kojo.doodle.Color
+import pixiscalajs.PIXI
+
+import scala.concurrent.Future
+
+abstract class BasePicSequece(pics: Seq[Picture]) extends Picture {
+  import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+  val tnode = new PIXI.Container()
+
+  pics.foreach { p =>
+    tnode.addChild(p.tnode)
+  }
+
+  def made: Boolean = {
+    val notMade = pics.find { p =>
+      !p.made
+    }
+    notMade match {
+      case Some(_) => false
+      case None    => true
+    }
+  }
+
+  lazy val ready: Future[Unit] = {
+    val futures = pics map (_.ready)
+    futures.reduce { (f1, f2) => for (_ <- f1; _ <- f2) yield () }
+  }
+
+  def realDraw(): Unit = {
+    println(ready.isCompleted)
+    kojoWorld.addLayer(tnode)
+  }
+
+  def initGeom() = {
+    var pg = pics(0).picGeom
+    pics.tail.foreach { pic =>
+      pg = pg union pic.picGeom
+    }
+    pg
+  }
+
+  def setFillColor(c: Color): Unit = {
+    pics.foreach { p =>
+      p.setFillColor(c)
+    }
+  }
+
+  def setPenColor(c: Color): Unit = {
+    pics.foreach { p =>
+      p.setPenColor(c)
+    }
+  }
+
+  def setPenThickness(t: Double): Unit = {
+    pics.foreach { p =>
+      p.setPenThickness(t)
+    }
+  }
+
+  def erase(): Unit = {
+    kojoWorld.removeLayer(tnode)
+  }
+}
+
+object GPics {
+  def apply(pics: collection.immutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new GPics(pics)
+  def apply(pics: collection.mutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new GPics(pics)
+  def apply(pics: Picture*)(implicit kojoWorld: KojoWorld) = new GPics(pics)
+}
+
+class GPics(pics: Seq[Picture])(implicit val kojoWorld: KojoWorld) extends BasePicSequece(pics) {
+}
+
+object HPics {
+  def apply(pics: collection.immutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new HPics(pics)
+  def apply(pics: collection.mutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new HPics(pics)
+  def apply(pics: Picture*)(implicit kojoWorld: KojoWorld) = new HPics(pics)
+}
+
+class HPics(pics: Seq[Picture])(implicit val kojoWorld: KojoWorld) extends BasePicSequece(pics) {
+  import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+  ready.foreach { _ =>
+    println(ready.isCompleted)
+    var ox = 0.0
+    pics.foreach { pic =>
+      pic.offset(ox, 0)
+      val nbounds = pic.bounds
+      ox = nbounds.x + nbounds.width
+    }
+  }
+}
+
+object HPicsCentered {
+  def apply(pics: collection.immutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new HPicsCentered(pics)
+  def apply(pics: collection.mutable.Seq[Picture])(implicit kojoWorld: KojoWorld) = new HPicsCentered(pics)
+  def apply(pics: Picture*)(implicit kojoWorld: KojoWorld) = new HPicsCentered(pics)
+}
+
+class HPicsCentered(pics: Seq[Picture])(implicit val kojoWorld: KojoWorld) extends BasePicSequece(pics) {
+  import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
+  ready.foreach { _ =>
+    var prevPic: Option[Picture] = None
+    pics.foreach { pic =>
+      prevPic match {
+        case Some(ppic) =>
+          val pbounds = ppic.bounds
+          val tx = pbounds.x + pbounds.width
+          pic.offset(tx, 0)
+          val bounds = pic.bounds
+          val ty = pbounds.y - bounds.y + (pbounds.height - bounds.height) / 2
+          val tx2 = pbounds.x + pbounds.width - bounds.x
+          pic.offset(tx2, ty)
+        case None =>
+      }
+      prevPic = Some(pic)
+    }
+  }
+}
