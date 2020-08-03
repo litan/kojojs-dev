6f1773cf67a0b7b2b17a8112b7081fe1ed7ce685
Lalit Pant <pant.lalit@gmail.com>
2020-08-03 23:10:19 +0530

    Update sync script.

--- sync-kojojs-core.sh
+++ sync-kojojs-core.sh
@@ -2,4 +2,6 @@ set -x o
 rm -rf ../kojojs-core/page/src/main/scala/kojo/
 cp -va src/main/scala/kojo/ ../kojojs-core/page/src/main/scala/
 cp -va src/main/scala/com/ ../kojojs-core/page/src/main/scala/
+cp -va src/main/scala/pixiscalajs/ ../kojojs-core/page/src/main/scala/
+cp -va src/main/scala/howlerscalajs/ ../kojojs-core/page/src/main/scala/
 meld src/main/scala/ ../kojojs-core/page/src/main/scala/
